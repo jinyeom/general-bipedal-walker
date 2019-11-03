084fda4428777934b90609ad98708de3ea2477cd
jinyeom <jinyeom95@gmail.com>
2019-11-03 01:17:15 -0500

    shared memory

--- general_bipedal_walker/vector.py
+++ general_bipedal_walker/vector.py
@@ -1,8 +1,17 @@
 import sys
+from gym.vector.utils import write_to_shared_memory
 
-def worker(index, env_fn, pipe, parent_pipe, shared_memory, error_queue):
-  assert shared_memory is None
+def _worker_shared_memory(
+    index, 
+    env_fn, 
+    pipe, 
+    parent_pipe, 
+    shared_memory, 
+    error_queue
+):
+  assert shared_memory is not None
   env = env_fn()
+  observation_space = env.observation_space
   parent_pipe.close()
   try:
     while True:
@@ -11,12 +20,24 @@ def worker(index, env_fn, pipe, parent_pipe, shared_memory, error_queue):
         env.sample(data)
       elif command == 'reset':
         observation = env.reset()
-        pipe.send((observation, True))
+        write_to_shared_memory(
+          index, 
+          observation, 
+          shared_memory, 
+          observation_space
+        )
+        pipe.send((None, True))
       elif command == 'step':
         observation, reward, done, info = env.step(data)
         if done:
           observation = env.reset()
-        pipe.send(((observation, reward, done, info), True))
+        write_to_shared_memory(
+          index, 
+          observation, 
+          shared_memory, 
+          observation_space
+        )
+        pipe.send(((None, reward, done, info), True))
       elif command == 'seed':
         env.seed(data)
         pipe.send((None, True))
@@ -24,7 +45,7 @@ def worker(index, env_fn, pipe, parent_pipe, shared_memory, error_queue):
         pipe.send((None, True))
         break
       elif command == '_check_observation_space':
-        pipe.send((data == env.observation_space, True))
+        pipe.send((data == observation_space, True))
       else:
         raise RuntimeError(
           'Received unknown command `{0}`. Must '
